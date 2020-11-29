4d98b9404d1b633fb460199c7c5be9acb366d1b3
Edwyn Rangel <edwynrangel@gmail.com>
2020-11-29 13:06:18 -0400

    refactoring

--- app.js
+++ app.js
@@ -5,32 +5,24 @@ function sumaSecuencia(array, base) {
     for (let i = 0; i < array.length; i++) {
     
         let acc = array[i];
-        let subArray = [array[i]];
+
+        if (acc === base) {
+            arrayFinal.push(array.slice(i, i + 1));
+        }
     
         for (let j = i + 1; j < array.length; j++) {
             acc += array[j];
-            if (acc >= base && array[j] < 0) {
-                subArray.push(array[j - 1]);
-                subArray.push(array[j]);
-                continue;
-            }
 
-            if (acc <= base) {
-                subArray.push(array[j]);
+            if (acc === base) {
+                arrayFinal.push(array.slice(i, j + 1));
             }
-
         }
-    
-        if (subArray.reduce((acc, cur) => acc += cur, 0) === 10) {
-            arrayFinal.push(subArray)
-        }
-    
     }
 
     return arrayFinal;
 }
 
-const array = [3, 5, 1, 8, -7, -2, 2, 1, 7, 2, 4, 5, 8, 2, 10];
+const array = [10, -10, 10, 8, -7, -2, 2, 1, 7, 2, 4, 5, 8, 2, 10];
 const base = 10;
 
 console.log(sumaSecuencia(array, base));
